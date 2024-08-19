e00b0e358c23884e25f265ba63ec35731e28ee96
Nikita Kostevich <nikitakostevich@gmail.com>
2024-08-19 14:49:00 +0300

    init: reverse number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const digitsArray = Math.abs(n).toString().split('');
+    const reversedArray = digitsArray.reverse();
+    const reversedn = parseInt(reversedArray.join(''), 10);
+
+    return reversedn;
 }
+
